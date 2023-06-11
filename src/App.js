095be3d56fbc2d668828d56deb17d56dc421170a
fadime <fadime.ozdemir11@gmail.com>
2023-06-11 17:02:38 +0300

    add pagination

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
 import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import './App.css';
 import { mergeSort } from "./helpers";
+import Pagination from './Pagination';
 
 function App() {
-  const [books, setBooks] = useState([]);
+  const [allBooks, setAllBooks] = useState([]);
+
+
+  useEffect(() => {
+    let isMounted = true;
+    fetch('https://raw.githubusercontent.com/topac/assignment/master/books.5000.json')
+      .then(async response => {
+        const fetchedBooks = await response.json();
+        if (isMounted && fetchedBooks) {
+          setAllBooks(fetchedBooks);
+        }
+      })
+      .catch(e => console.error("error during fetch", e))
+
+    return () => {
+      isMounted = false;
+      setAllBooks([]);
+    };
+  }, []);
+
+
+  // Handle displayed books  
+  const [displayedBooks, setDisplayedBooks] = useState([]);
+  const [currentPage, setCurrentPage] = useState({ limit: 20, offset: 0 });
+  useEffect(() => {
+    // In the table the books currently displayed are between the offset and limit numbers.
+    // The offset is the number of books from the first book in the array, while the limit is the offset book + 20 books.
+    // The 20 books are those which are displayed in the table.
+    setDisplayedBooks(allBooks.slice(currentPage.offset, currentPage.limit));
+    return () => setDisplayedBooks([]);
+  }, [allBooks, currentPage]);
+
   const bookRows = useMemo(() => {
-    return books.map((book) => {
+    return displayedBooks.map((book) => {
       return <tr key={book.Isbn}>
         <td>
           {book.Isbn}
@@ -24,36 +56,19 @@ function App() {
         </td>
       </tr>
     })
-  }, [books]);
+  }, [displayedBooks]);
+
 
   const sortCol = useCallback((colName, order, type) => {
-    const sortByColName = (books) => {
-      const sortedBooks = mergeSort(books, colName, order, type);
+    const sortByColName = (allBooks) => {
+      const sortedBooks = mergeSort(allBooks, colName, order, type);
       return sortedBooks;
     }
-    setBooks(sortByColName);
+    setAllBooks(sortByColName);
   }, []);
 
-  useEffect(() => {
-    let isMounted = true;
-    fetch("https://raw.githubusercontent.com/topac/assignment/master/books.5000.json")
-      .then(async response => {
-        const fetchedBooks = await response.json();
-        if (isMounted) {
-          setBooks(fetchedBooks);
-          window.localStorage.setItem('books', JSON.stringify(fetchedBooks));
-        }
-      })
-      .catch(e => console.error("error during fetch", e))
-
-    return () => {
-      isMounted = false;
-      setBooks([])
-    };
-  }, []);
 
   const activeButtonRef = useRef(null);
-
   const tabHeaders = useMemo(() => {
     return [{ title: "Isbn", type: "number" }, { title: "Title", type: "string" }, { title: "Author", type: "string" }, { title: "Year", type: "number" }, { title: "Publisher", type: "string" }].map(({ title, type }, i) => {
       const handleSort = (event, sortedList) => {
@@ -89,6 +104,7 @@ function App() {
           {bookRows}
         </tbody>
       </table>
+      <Pagination setCurrentPage={setCurrentPage} totPages={allBooks?.length} />
     </div>
   );
 }
---
+++ src/Pagination.js
@@ -0,0 +1,27 @@
+import React, { useRef } from 'react';
+
+export default function Pagination({ totPages, setCurrentPage }) {
+    const activePage = useRef(null);
+
+
+    const handlePageChange = (event) => (pageNum) => {
+        const firstBtn = document.getElementById('page-0');
+        firstBtn.classList.remove('active');
+
+        if (activePage.current) {
+            activePage.current.classList.remove('active');
+        }
+        event.target.classList.add('active');
+        activePage.current = event.target;
+        const startingBookIndex = 20 * pageNum; // index of the book we start displaying from.
+        setCurrentPage(s => ({ limit: startingBookIndex + 20, offset: startingBookIndex }))
+    };
+
+
+    const pages = Array.from({ length: Math.floor(totPages / 20) }).map((_, num) => <button ref={activePage} key={'pagination-' + num} onClick={(event) => handlePageChange(event)(num)} id={'page-' + num} className={num === 0 ? 'active pagination-btn flex items-center justify-center' : 'pagination-btn flex items-center justify-center'}>{num}</button>)
+    return (
+        <div className='flex pagination flex-wrap justify-between' style={{ width: 700 }}>
+            {pages}
+        </div>
+    )
+}

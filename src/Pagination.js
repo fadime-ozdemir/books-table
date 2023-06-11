104c9287e5b689b1a31742d4ee5a9a96baf12353
fadime <fadime.ozdemir11@gmail.com>
2023-06-11 17:18:15 +0300

    fix pagination text

--- src/Pagination.js
+++ src/Pagination.js
@@ -18,7 +18,7 @@ export default function Pagination({ totPages, setCurrentPage }) {
     };
 
 
-    const pages = Array.from({ length: Math.floor(totPages / 20) }).map((_, num) => <button ref={activePage} key={'pagination-' + num} onClick={(event) => handlePageChange(event)(num)} id={'page-' + num} className={num === 0 ? 'active pagination-btn flex items-center justify-center' : 'pagination-btn flex items-center justify-center'}>{num}</button>)
+    const pages = Array.from({ length: Math.floor(totPages / 20) }).map((_, num) => <button ref={activePage} key={'pagination-' + num} onClick={(event) => handlePageChange(event)(num)} id={'page-' + num} className={num === 0 ? 'active pagination-btn flex items-center justify-center' : 'pagination-btn flex items-center justify-center'}>{(20 * num) + 1 + "-" + ((20 * num) + 20)}</button>)
     return (<>
         <h4 className='pagination-label'>Pages:</h4>
         <div className='flex pagination flex-wrap justify-between' style={{ width: 700 }}>
